6c29138b4f19c385cb7725b3e09a5785267b4ec0
Azat Seyrek <azatseyrek@hotmail.com>
2022-02-03 00:57:12 +0300

    cleaning react

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,20 +7,25 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+import User from "./User";
+import { DatabaseUserInterface, UserInterface } from "./interfaces/UserInterface";
+
+const LocalStrategy = passportLocal.Strategy
+
+dotenv.config();
 
 mongoose.connect(
   "mongodb+srv://azatseyrek:admin@cluster0.tmqvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   (err: Error) => {
     if (err) throw err;
     console.log("connected to mongo");
-    
   }
 );
 
 // Middleware
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(
   session({
     secret: "secretcode",
@@ -31,3 +36,78 @@ app.use(
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Passport 
+passport.use(new LocalStrategy((username: string, password: string, done) => {
+    User.findOne({ username: username }, (err:Error, user: DatabaseUserInterface) => {
+      if (err) throw err;
+      if (!user) return done(null, false);
+      bcrypt.compare(password, user.password, (err, result: boolean) => {
+        if (err) throw err;
+        if (result === true) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    });
+  })
+  );
+  
+  passport.serializeUser((user: DatabaseUserInterface, cb) => {
+    cb(null, user._id);
+  });
+  
+  passport.deserializeUser((id: string, cb) => {
+    User.findOne({ _id: id }, (err: Error, user: DatabaseUserInterface) => {
+      const userInformation: UserInterface = {
+        username: user.username,
+        isAdmin: user.isAdmin,
+        id: user._id
+      };
+      cb(err, userInformation);
+    });
+  });
+  
+
+// Routes
+app.post("/register", async (req, res) => {
+  const { username, password } = req?.body;
+  // validation
+  if (
+    !username ||
+    !password ||
+    typeof username !== "string" ||
+    typeof password !== "string"
+  ) {
+    res.send("Improper Values");
+    return;
+  }
+  User.findOne({ username }, async (err: Error, doc: DatabaseUserInterface) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("success");
+    }
+  });
+});
+
+
+app.post("/login", passport.authenticate("local"), (req: Request, res:Response) => {
+    res.send("success")
+  });
+  
+  app.get("/user", (req, res) => {
+    res.send(req.user);
+  });
+  
+
+app.listen(4000, () => {
+  console.log("Server Started");
+});
